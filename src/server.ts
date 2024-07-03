15afd70116f009d11c2c3d096b9e56c62f014f55
shahisrail <shahisrail134@gmail.com>
2024-07-03 06:36:58 +0600

    uncomment server console

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,10 @@ async function main() {
         await mongoose.connect(config.database_url as string);
         // console.log( 'mongodb connect');
         app.listen(config.port, () => {
-            // console.log(`Example app listening on port ${config.port}`);
+            console.log(`Example app listening on port ${config.port}`);
         });
     } catch (error) {
-        // console.log(error);
+        console.log(error);
     }
 }
 main();
